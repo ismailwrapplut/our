6273dc6c511558fbcb4ce85d39ab3b977017eb16
faizfinancial <72145659+faizfinancial@users.noreply.github.com>
2022-10-09 19:30:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyttsx3 #pip install pyttsx3
 import speech_recognition as sr #pip install speechRecognition
 import datetime
 import wikipedia #pip install wikipedia
-import webbrowser
+import webbrowser mera bhai tu meri jaan hai
 import os
 import smtplib
 
