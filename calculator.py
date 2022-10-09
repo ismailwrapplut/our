dea43cd6e790a68aee215d7d4a7564a9e22a1043
faizfinancial <72145659+faizfinancial@users.noreply.github.com>
2022-10-09 19:21:13 +0530

    Update my changes

--- calculator.py
+++ calculator.py
@@ -2,4 +2,4 @@ print("Enter your first number")
 a=int(input())
 print("Enter your second number")
 b=int(input())#it is good one
-#i think you should add division
+#i think you should add division yes you are writ
